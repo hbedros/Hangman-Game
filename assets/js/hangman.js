0df0747d539292dcc1ba50ce158ac19eb4ed864e
haigbedros <haigbedros@gmail.com>
2018-01-01 19:39:37 -0800

    JS arrays array with the right guessed letters array with the wrong guessed letters replacing underscores with right letters

--- assets/js/hangman.js
+++ assets/js/hangman.js
@@ -1,24 +1,46 @@
-// create an array of words
+//Create an array of words
 const words = ['red', 'blue', 'orange'];
-// choose word randomly
-let randNum  = Math.floor(Math.random() * words.length);
-let chosenWords = words[randNum];
-console.log(chosenWords);
+
+//Choose word randomly
+let randomNumber  = Math.floor(Math.random() * words.length);
+let rightLetters = [];
+let wrongLetters = [];
+let chosenWords = words[randomNumber];
 let underScore = [];
-//create underscores based on length of words
+console.log(chosenWords);
+
+//Create underscores based on length of words
 function generateUnderScore() {
     for(let i =0; i < chosenWords.length; i++){
         underScore.push('_');
     }
    return underScore;     
 }
-
 console.log(generateUnderScore());
+
 //Get users guess
 document.addEventListener('keypress', function() {
-    let pressedKeyCode = event.keyCode;
-    console.log(pressedKeyCode);
+    let pressedKeyLetter = String.fromCharCode(event.keyCode);
+    // console.log(pressedKeyLetter);
+
+//if users guess is rigth 
+    if(chosenWords.indexOf(pressedKeyLetter) > -1) {
+        rightLetters.push(pressedKeyLetter);
+        //replace underscore with right letters
+        underScore[chosenWords.indexOf(pressedKeyLetter)] = pressedKeyLetter;
+        console.log(underScore);
+        if(underScore.join('') == chosenWords) {
+            alert('you won');
+        }
+
+
+    }else {
+        wrongLetters.push(pressedKeyLetter);
+        console.log(wrongLetters);
+    }
+    
 });
 //check if guess is right
 //if right push to rights array
 //if wrong push to wrong array
+ 
