2199335ee553c18b46ea5bda6252741ee8edd0b7
haigbedros <haigbedros@gmail.com>
2018-01-01 21:26:20 -0800

    DOM manupilation replace the right letter with the underscores add the wrong guessed letters in the wrong guesses field

--- assets/js/hangman.js
+++ assets/js/hangman.js
@@ -3,16 +3,24 @@ const words = ['red', 'blue', 'orange'];
 
 //Choose word randomly
 let randomNumber  = Math.floor(Math.random() * words.length);
+let wins = 0;
+let losses = 0;
 let rightLetters = [];
 let wrongLetters = [];
 let chosenWords = words[randomNumber];
 let underScore = [];
 console.log(chosenWords);
 
+//DOM manipulation
+let docUnderScore = document.getElementsByClassName('underscore')
+let docRightGuess = document.getElementsByClassName('right_guess')
+let docWrongGuess = document.getElementsByClassName('wrong_guess')
+
 //Create underscores based on length of words
 function generateUnderScore() {
     for(let i =0; i < chosenWords.length; i++){
         underScore.push('_');
+
     }
    return underScore;     
 }
@@ -26,8 +34,10 @@ document.addEventListener('keypress', function() {
 //if users guess is rigth 
     if(chosenWords.indexOf(pressedKeyLetter) > -1) {
         rightLetters.push(pressedKeyLetter);
+
         //replace underscore with right letters
         underScore[chosenWords.indexOf(pressedKeyLetter)] = pressedKeyLetter;
+        docUnderScore[0].innerHTML = underScore.join(' ');
         console.log(underScore);
         if(underScore.join('') == chosenWords) {
             alert('you won');
@@ -36,10 +46,14 @@ document.addEventListener('keypress', function() {
 
     }else {
         wrongLetters.push(pressedKeyLetter);
+        docWrongGuess[0].innerHTML = wrongLetters.join(' , ');
         console.log(wrongLetters);
     }
     
 });
+
+docUnderScore[0].innerHTML = underScore.join(' ');
+
 //check if guess is right
 //if right push to rights array
 //if wrong push to wrong array
